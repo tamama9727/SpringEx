73e7dec82be62fe98705683fb830bf8b7cf1994e
DESKTOP-O403M3T\user <ta>
2022-07-04 21:28:45 +0900

    [권성진] -Spring 최초 생성

---
+++ src/main/java/com/gunn/ex/Hello/HelloController.java
@@ -0,0 +1,16 @@
+package com.gunn.ex.Hello;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+	
+	@ResponseBody
+	@RequestMapping("/hello")
+	public String HelloWorld() {
+		return "Hello World";
+	}
+
+}
